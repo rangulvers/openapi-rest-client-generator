aaca9857c1fa547ae8fec5677ac9ec12effce7f8
rangulvers <malte.sohns@googlemail.com>
2024-01-18 16:46:27 +0100

    version='0.0.1'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gen-openapi-rest',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     description='Generate .http files from OpenAPI/Swagger JSON. to be used with VS Code REST Client.',
     long_description='Generate .http files from OpenAPI/Swagger JSON. to be used with VS Code REST Client',
