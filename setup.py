2ecd7ee0e90597963efd338858fd1fb297a3bf0f
rangulvers <malte.sohns@googlemail.com>
2024-01-18 16:40:51 +0100

    changed name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='openapi-gen',
+    name='gen-openapi-rest',
     version='0.1',
     packages=find_packages(),
     description='Generate .http files from OpenAPI/Swagger JSON. to be used with VS Code REST Client.',
