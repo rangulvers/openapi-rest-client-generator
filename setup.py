ae452dfef2cc7bdd02031f0f12d961d8a400b051
rangulvers <malte.sohns@googlemail.com>
2024-01-31 15:39:18 +0100

    version3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("readme.md", "r", encoding="utf-8") as f:
 
 setup(
     name="gen-openapi-rest",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     description="Generate .http files from OpenAPI/Swagger JSON",
     long_description=long_description,
