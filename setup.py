624949f8af58ac2d8da6ff12fce5f42397186342
rangulvers <malte.sohns@googlemail.com>
2024-01-31 15:00:36 +0100

    format

--- setup.py
+++ setup.py
@@ -4,27 +4,25 @@
 from setuptools import find_packages, setup
 
 # read the contents of your README file and add it to the long_description
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='gen_openapi_rest',
-    version='0.0.1',
+    name="gen_openapi_rest",
+    version="0.0.1",
     packages=find_packages(),
-    description='Generate .http files from OpenAPI/Swagger JSON',
+    description="Generate .http files from OpenAPI/Swagger JSON",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='rangulvers',
-    url='https://github.com/rangulvers/openapi-rest-client-generator',
-    license='LICENSE',
-    install_requires=[
-        'requests'
-    ],
+    long_description_content_type="text/markdown",
+    author="rangulvers",
+    url="https://github.com/rangulvers/openapi-rest-client-generator",
+    license="LICENSE",
+    install_requires=["requests"],
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.org/classifiers/
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
 )
