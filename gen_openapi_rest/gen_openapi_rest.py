91297624c77ad2599bd23f09904efdb12073c9e6
rangulvers <malte.sohns@googlemail.com>
2024-01-31 14:59:22 +0100

    update format

--- gen_openapi_rest/gen_openapi_rest.py
+++ gen_openapi_rest/gen_openapi_rest.py
@@ -1,6 +1,7 @@
 """Generates a .http file from a Swagger JSON file.
 
 """
+
 # pylint: disable=line-too-long
 # pylint: disable=broad-exception-caught
 # pylint: disable=invalid-name
@@ -18,11 +19,11 @@ def fetch_swagger_json(source):
     Fetches the Swagger JSON from a URL or a local file.
     """
     parsed_url = urlparse(source)
-    if parsed_url.scheme in ('http', 'https'):
+    if parsed_url.scheme in ("http", "https"):
         response = requests.get(source, timeout=60)
         response.raise_for_status()
         return response.json()
-    with open(source, 'r', encoding="utf-8") as f:
+    with open(source, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
@@ -34,33 +35,33 @@ def generate_http_file(swagger_json_input, output_file_input):
         output_file (_type_): _description_
     """
     # Check for OpenAPI version and handle 'servers' or 'host' + 'basePath'
-    if swagger_json_input.get('openapi', '').startswith('3.'):
+    if swagger_json_input.get("openapi", "").startswith("3."):
         # OpenAPI 3.x.x
-        servers = swagger_json_input.get('servers', [])
-        base_url = servers[0]['url'] if servers else ''
+        servers = swagger_json_input.get("servers", [])
+        base_url = servers[0]["url"] if servers else ""
     else:
         # Swagger 2.0
-        host = swagger_json_input.get('host', 'localhost')
-        base_path = ensure_trailing_slash(swagger_json_input.get('basePath', '/'))
-        schemes = swagger_json_input.get('schemes', ['http'])
+        host = swagger_json_input.get("host", "localhost")
+        base_path = ensure_trailing_slash(swagger_json_input.get("basePath", "/"))
+        schemes = swagger_json_input.get("schemes", ["http"])
         base_url = f"{schemes[0]}://{host}{base_path}"
     with open(output_file_input, "w", encoding="utf-8") as f:
-        for path, methods in swagger_json_input['paths'].items():
+        for path, methods in swagger_json_input["paths"].items():
             for method, details in methods.items():
                 request_url = build_request_url(base_url, path)
-                parameters = details.get('parameters', [])
+                parameters = details.get("parameters", [])
                 comments, query_params = process_parameters(parameters)
                 if query_params:
-                    request_url += '?' + '&'.join(query_params)
+                    request_url += "?" + "&".join(query_params)
                 request = create_request(method.upper(), request_url, details, comments)
-                f.write(request + '\n\n')
+                f.write(request + "\n\n")
 
 
 def ensure_trailing_slash(path):
     """
     Ensures that the path ends with a slash.
     """
-    return path if path.endswith('/') else path + '/'
+    return path if path.endswith("/") else path + "/"
 
 
 def build_request_url(base_url, path):
@@ -77,12 +78,12 @@ def process_parameters(parameters):
     comments = []
     query_params = []
     for param in parameters:
-        if param['in'] == 'query':
+        if param["in"] == "query":
             param_value = f"{{{param['name']}}}"
-            if param.get('type') == 'array' and 'enum' in param['items']:
-                options = ', '.join(param['items']['enum'])
+            if param.get("type") == "array" and "enum" in param["items"]:
+                options = ", ".join(param["items"]["enum"])
                 comments.append(f"# {param['name']} options: {options}")
-                example_option = param['items']['enum'][0]
+                example_option = param["items"]["enum"][0]
                 param_value = example_option
             query_params.append(f"{param['name']}={param_value}")
     return comments, query_params
@@ -92,23 +93,23 @@ def create_request(method, request_url, details, comments):
     """
     Creates the HTTP request text.
     """
-    headers = create_headers(details.get('parameters', []))
-    body = create_body(details.get('parameters', []))
-    comments_section = '\n'.join(comments) + '\n' if comments else ''
+    headers = create_headers(details.get("parameters", []))
+    body = create_body(details.get("parameters", []))
+    comments_section = "\n".join(comments) + "\n" if comments else ""
     request_line = (
         f"###\n# {details.get('summary', 'No summary')}\n"
         f"{comments_section}{method} {request_url} HTTP/1.1\n"
-        )
-    return request_line + headers + '\n' + body
+    )
+    return request_line + headers + "\n" + body
 
 
 def create_headers(parameters):
     """
     Creates the headers section of the HTTP request.
     """
-    headers = ''
+    headers = ""
     for parameter in parameters:
-        if parameter['in'] == 'header':
+        if parameter["in"] == "header":
             headers += (
                 f"{parameter['name']}:"
                 f"{parameter.get('default','{'+parameter['name']+'}')}\n"
@@ -120,11 +121,11 @@ def create_body(parameters):
     """
     Creates the body of the HTTP request.
     """
-    body = ''
+    body = ""
     for parameter in parameters:
-        if parameter['in'] == 'body':
+        if parameter["in"] == "body":
             # Assuming the body is JSON. You might need to handle other types as well.
-            body = json.dumps(parameter['schema'], indent=2)
+            body = json.dumps(parameter["schema"], indent=2)
     return body
 
 
@@ -137,6 +138,6 @@ if __name__ == "__main__":
     try:
         swagger_json = fetch_swagger_json(swagger_json_source)
         generate_http_file(swagger_json, output_file)
-        print(f'Successfully created {output_file} from {swagger_json_source}')
+        print(f"Successfully created {output_file} from {swagger_json_source}")
     except Exception as e:
         print(f"An error occurred: {e}")
