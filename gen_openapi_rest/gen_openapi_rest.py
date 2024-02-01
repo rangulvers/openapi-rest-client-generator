e141de4954326150cdb82b69a7a6d6dbe6bbe82c
rangulvers <malte.sohns@googlemail.com>
2024-02-01 12:29:05 +0100

    remove old colde

--- gen_openapi_rest/gen_openapi_rest.py
+++ gen_openapi_rest/gen_openapi_rest.py
@@ -126,17 +126,3 @@ def create_body(parameters):
             # Assuming the body is JSON. You might need to handle other types as well.
             body = json.dumps(parameter["schema"], indent=2)
     return body
-
-
-# if __name__ == "__main__":
-#     if len(sys.argv) != 3:
-#         print("Usage: python script.py <swagger-json-source> <output-http-file>")
-#         sys.exit(1)
-#     swagger_json_source = sys.argv[1]
-#     output_file = sys.argv[2]
-#     try:
-#         swagger_json = fetch_swagger_json(swagger_json_source)
-#         generate_http_file(swagger_json, output_file)
-#         print(f"Successfully created {output_file} from {swagger_json_source}")
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
